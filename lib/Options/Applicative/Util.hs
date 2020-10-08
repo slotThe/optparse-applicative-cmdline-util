72ee197ab0c2ad53e1204ed45f77269901f6d589
slotThe <soliditsallgood@mailbox.org>
2020-10-08 18:07:47 +0200

    Add a little bit of module documentation
    This gives a little bit of an overview of the kind of functions that are in this
    library so far.

--- lib/Options/Applicative/Util.hs
+++ lib/Options/Applicative/Util.hs
@@ -6,6 +6,20 @@
    Maintainer  : slotThe <soliditsallgood@mailbox.org>
    Stability   : experimental
    Portability : non-portable
+
+This module contains utility functions for working with the
+'optparse-applicative' library.
+
+Much of the module revolves around easily building options that can take
+"multiple arguments" in the form of separated inputs (e.g. program
+--option one,two,three,four).  This still honours the POSIX standard for
+options only taking a single argument (by not using spaces to separate
+the different inputs), while also being very convenient to enter (as
+opposed to, say, wrapping everything inside quotes).
+
+Another focus involves connecting the 'attoparsec' library with
+'optparse-applicative' (this is often useful when options involve more
+complex parsing patterns).
 -}
 module Options.Applicative.Util
     ( -- * Types
